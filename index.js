9013612199330a3d60875c3e4cee3943e3e7d405
Lucien <lucien.zak@laplateforme.io>
2022-12-06 14:16:16 +0100

    integration JWT, authentification, route inscription au groupe

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-const { User } = require('./models');
+const userRoute = require('./routes/user');
+const groupRoute = require('./routes/group');
+
 
 //TODO
 // - XXXXXXXUne route qui permet à un utilisateur de s’enregistrer en complétant les champs
@@ -8,7 +10,7 @@ const { User } = require('./models');
 // - XXXXXX Une route qui permet à l’utilisateur de se connecter
 // - XXXXXX Une route qui retourne tous les utilisateurs dans une liste contenant les champs
 // prenom et nom.
-// - Une route qui retourne uniquement les noms des groupes (groupe 1, groupe 2, ...)
+// - XXXX Une route qui retourne uniquement les noms des groupes (groupe 1, groupe 2, ...)
 // - Une route qui retourne les groupes ainsi que les users qui y sont rattachés
 // (uniquement prenom et nom)
 // - Une route qui permet d’avoir les détails d’un user (nom prenom email groupe)
@@ -24,63 +26,7 @@ app.use(express.json());
 app.get('/', function(req, res) {
   res.send('hello world');
 });
-
-//Création utilisateur 
-// {
-//   "firstname" : "testfirstname",
-//   "lastname" : "testlastname",
-//   "email" : "testemail",
-//   "password" : "testpassword",
-//   "groupId" : 1
-// }
-app.post('/api/users', async (req, res) => {
-  const { firstName, lastName, email, password, groupId } = req.body;
-  try {
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password,
-      groupId
-    });
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json(error.errors);
-  }
-    
-});
-
-//Connexion utilisateur
-// {
-//   "email" : "testemail",
-//   "password" : "testpassword"
-// }
-app.post('/api/users/login', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({
-      where: {
-        email,
-        password
-      }
-    });
-    if (user) {
-      res.status(200).json({ message : "Vous êtes connecté"});
-    }
-    else {
-      res.status(404).json({ message: 'Erreur sur le login' });
-    }
-  }
-  catch (error) {
-    res.json(error.errors);
-  };
-});
-
-app.get("/api/users", async (req, res) => {
-  const users = await User.findAll({
-    attributes: ['firstName', 'lastName']
-});
-  res.json(users);
-});
+app.use('/api/users', userRoute);
+app.use('/api/groups', groupRoute);
 
 app.listen(3000, () => console.log('Serveur lancé sur le port 3000 😍'));
